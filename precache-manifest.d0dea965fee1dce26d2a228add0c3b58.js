ba2435f14b7d47f73ea7a0839e61505ed19dcf9d
paulangcrackers <bmariepaula121@gmail.com>
2020-02-28 15:52:48 +0800

    Updates

---
+++ precache-manifest.d0dea965fee1dce26d2a228add0c3b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d21b114835ed227086a27bc7063d0fd",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "7237ab56761ca39fdcd2",
+    "url": "/budget-calculator/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "e95c1d1eba76840167bb",
+    "url": "/budget-calculator/static/css/main.79cb166d.chunk.css"
+  },
+  {
+    "revision": "7237ab56761ca39fdcd2",
+    "url": "/budget-calculator/static/js/2.6869619e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/budget-calculator/static/js/2.6869619e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95c1d1eba76840167bb",
+    "url": "/budget-calculator/static/js/main.dfeba871.chunk.js"
+  },
+  {
+    "revision": "cf456d8ec70753a7c657",
+    "url": "/budget-calculator/static/js/runtime-main.c9cd4332.js"
+  }
+]);
